0b4e0f164a18bac99af1d2c88b9e3101f064f1de
Dominik Schmidt <schmidt.dominik@uni-ulm.de>
2026-04-27 19:06:16 +0200

    feat: testing start

---
+++ src/__tests__/health.test.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+import healthRoute from '../routes/health'
+
+describe('GET /health', () => {
+    it('returns status ok', async () => {
+        const app = Fastify()
+        app.register(healthRoute)
+        
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.json()).toEqual({ status: 'ok' })
+    })
+})
